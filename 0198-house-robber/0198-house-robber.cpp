c320dec1d9dc67b0e4245ffa461d39881f87bd86
MANAS RAJ <manasraj_23cs238@dtu.ac.in>
2026-01-12 22:56:56 +0530

    Time: 0 ms (100%), Space: 10.7 MB (43.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int rec(int i, int n, vector<int>&nums,vector<int>&dp){
-        if(i>=n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int skip=rec(i+1,n,nums,dp);
-        int take=nums[i]+rec(i+2,n,nums,dp);
-        return dp[i]=max(skip,take);
-    }
+    // int rec(int i, int n, vector<int>&nums,vector<int>&dp){
+    //     if(i>=n)return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     int skip=rec(i+1,n,nums,dp);
+    //     int take=nums[i]+rec(i+2,n,nums,dp);
+    //     return dp[i]=max(skip,take);
+    // }
+
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return rec(0,n,nums,dp);
+        if(n==1) return nums[0];
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
         
     }
 };
