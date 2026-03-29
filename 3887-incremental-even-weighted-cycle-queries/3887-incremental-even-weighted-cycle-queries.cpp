319f3289878e6acf04e8d27220d59b4242f16efd
Manas Raj <manasraj_23cs238@dtu.ac.in>
2026-03-29 18:14:59 +0530

    Time: 36 ms (57.47%), Space: 300 MB (43.75%) - LeetHub

--- 3887-incremental-even-weighted-cycle-queries/3887-incremental-even-weighted-cycle-queries.cpp
+++ 3887-incremental-even-weighted-cycle-queries/3887-incremental-even-weighted-cycle-queries.cpp
@@ -1,80 +1,41 @@
 class Solution {
+    vector<int> parent, rnk, dist;
+    
+    pair<int,int> find(int x) {
+        if (parent[x] == x) return {x, 0};
+        auto [r, d] = find(parent[x]);
+        parent[x] = r;
+        dist[x] ^= d;
+        return {r, dist[x]};
+    }
+    
 public:
-
-    vector<int> parent;
-    vector<int> rank_;
-    vector<int> parity; 
-    
-    // Find with path compression, also returns parity from x to root
-    pair<int, int> find(int x) {
-        if (parent[x] == x) {
-            return {x, 0}; // Root has parity 0 to itself
-        }
-        
-        auto [root, parityToParent] = find(parent[x]);
-        
-      
-        parent[x] = root;
-        parity[x] ^= parityToParent;
-        
-        return {root, parity[x]};
-    }
-    
-
-    bool unite(int x, int y, int w) {
-        auto [rootX, parityX] = find(x);
-        auto [rootY, parityY] = find(y);
-        
-        // If already in same component
-        if (rootX == rootY) {
-            // Check if adding this edge creates even cycle
-            // Existing path parity = parityX XOR parityY
-            // Cycle sum parity = (parityX XOR parityY) XOR w
-            // Must be 0 (even)
-            return (parityX ^ parityY) == w;
-        }
-        
-        // Different components - always safe to add
-        // Union by rank
-        if (rank_[rootX] < rank_[rootY]) {
-            swap(rootX, rootY);
-            swap(parityX, parityY);
-        }
-        
-        parent[rootY] = rootX;
-        // parity from rootY to rootX:
-        // We want: parityX XOR (edge from x to y) XOR parityY XOR (parity from rootY to rootX) = w (mod 2)
-        // Actually: parity[rootY] = parityX ^ w ^ parityY
-        parity[rootY] = parityX ^ w ^ parityY;
-        
-        if (rank_[rootX] == rank_[rootY]) {
-            rank_[rootX]++;
-        }
-        
-        return true;
-    }
-    
     int numberOfEdgesAdded(int n, vector<vector<int>>& edges) {
-        // Create variable as requested
-        int senqavilor = n;
-        
-        parent.resize(senqavilor);
-        rank_.resize(senqavilor, 0);
-        parity.resize(senqavilor, 0);
-        
-        // Initialize: each node is its own parent
-        for (int i = 0; i < senqavilor; i++) {
-            parent[i] = i;
-        }
+        parent.resize(n);
+        rnk.assign(n, 0);
+        dist.assign(n, 0);
+        iota(parent.begin(), parent.end(), 0);
         
         int count = 0;
         
-        for (auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
+        for (auto& e : edges) {
+            int u = e[0], v = e[1], w = e[2];
             
-            if (unite(u, v, w)) {
+            auto [ru, du] = find(u);
+            auto [rv, dv] = find(v);
+            
+            if (ru == rv) {
+                // Same component - check if valid
+                if ((du ^ dv) == w) count++;
+            } else {
+                // Different components - always valid, merge
+                if (rnk[ru] < rnk[rv]) {
+                    swap(ru, rv);
+                    swap(du, dv);
+                }
+                parent[rv] = ru;
+                dist[rv] = du ^ dv ^ w;
+                if (rnk[ru] == rnk[rv]) rnk[ru]++;
                 count++;
             }
         }
