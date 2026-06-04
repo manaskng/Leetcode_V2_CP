6b325f4d38d011d156a3200a9efd9ae13e741c5f
Manas Raj <manasraj_23cs238@dtu.ac.in>
2026-06-04 22:29:19 +0530

    Time: 23 ms (29.35%), Space: 38.7 MB (27.76%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>ans;
+        priority_queue<pair<int,int>>pq;
+
+        for(int i=0;i<arr.size();i++){
+            pq.push({abs(arr[i]-x),arr[i]});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        while(!pq.empty()){
+            auto  topi=pq.top();
+            ans.push_back(topi.second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
